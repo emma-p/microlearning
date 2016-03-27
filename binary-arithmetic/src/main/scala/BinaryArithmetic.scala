1b778ef1dabcb268816f7fa8f768dfc9e25de338
Emmanuelle Poirier <manuepoirier@gmail.com>
2016-03-27 21:12:30 +0100

    Sum should work for numbers of different length

--- binary-arithmetic/src/main/scala/BinaryArithmetic.scala
+++ binary-arithmetic/src/main/scala/BinaryArithmetic.scala
@@ -1,17 +1,19 @@
-case class Result(data: Array[Char], carry: Char)
+case class Result(data: Vector[Char], carry: Char)
 
 object BinaryArithmetic {
-  def sum(n1: Array[Char], n2: Array[Char]): Option[Array[Char]] = {
+  def sum(v1: Vector[Char], v2: Vector[Char]): Option[Vector[Char]] = {
+    val (n1, n2) = equalizeNumbers(v1, v2)
     if (isBinary(n1 ++ n2)) {
-      val totalSum = n1.zip(n2).foldRight(Result(Array[Char](), '0')) { case ((charAtN1, charAtN2), acc) => {
+
+      val totalSum = n1.zip(n2).foldRight(Result(Vector[Char](), '0')) { case ((charAtN1, charAtN2), acc) => {
         val bits = sumBits(charAtN1, charAtN2, acc.carry)
         Result(bits._1 +: acc.data, bits._2)
       } }
+
       if(totalSum.carry == '1') {
         Some('1' +: totalSum.data)
-      } else {
-        Some(totalSum.data)
-      }
+      } else Some(totalSum.data)
+
     } else None
   }
 
@@ -26,16 +28,28 @@ object BinaryArithmetic {
   }
 
 
-  def toDouble(number: Array[Char]): Double = {
+  def toDouble(number: Vector[Char]): Double = {
     number.reverse.zipWithIndex.foldLeft(0.0) { case (result, (c, i)) =>
       result + (Integer.parseInt(c.toString) * Math.pow(2, i))
     }
   }
 
+  private def equalizeNumbers(n1: Vector[Char], n2: Vector[Char]): (Vector[Char], Vector[Char]) = {
+    val diff = n1.lengthCompare(n2.length)
+
+    // additionalZeros will be empty if the two lengths are equal
+    val additionalZeros = Vector.fill(Math.abs(diff)) { '0' }
+
+    if (diff >= 0) {
+      (n1, additionalZeros ++ n2)
+    } else (additionalZeros ++ n1, n2)
+  }
+
   private def charToInt(c: Char): Int = {
     Integer.parseInt(c.toString)
   }
-  private def isBinary(n: Array[Char]): Boolean = {
+
+  private def isBinary(n: Vector[Char]): Boolean = {
     n.filterNot(c => c == '1' || c == '0').isEmpty
   }
 }
--- binary-arithmetic/src/test/scala/BinaryArithmeticTest.scala
+++ binary-arithmetic/src/test/scala/BinaryArithmeticTest.scala
@@ -7,22 +7,34 @@ class BinaryArithmeticTest extends FlatSpec with Matchers {
     val n1 = "101010"
     val n2 = "111111"
 
-    val parsedn1 = Integer.parseInt(n1, 2)
-    val parsedn2 = Integer.parseInt(n2, 2)
+    val parsedN1 = Integer.parseInt(n1, 2)
+    val parsedN2 = Integer.parseInt(n2, 2)
 
-    val result = BinaryArithmetic.sum(n1.toCharArray,n2.toCharArray).map(r => Integer.parseInt(r.mkString(""), 2))
-    result should be(Some(parsedn1 + parsedn2))
+    val result = BinaryArithmetic.sum(n1.toVector,n2.toVector).map(r => Integer.parseInt(r.mkString(""), 2))
+    result should be(Some(parsedN1 + parsedN2))
   }
 
   it should "fail if the string does not only contain 1s and 0s" in {
     val n1 = "101010"
     val n2 = "121111"
-    val result = BinaryArithmetic.sum(n1.toCharArray,n2.toCharArray).map(r => Integer.parseInt(r.mkString(""), 2))
+    val result = BinaryArithmetic.sum(n1.toVector,n2.toVector).map(r => Integer.parseInt(r.mkString(""), 2))
     result should be(None)
   }
 
+  it should "work for binary numbers of different length" in {
+    val n1 = "101"
+    val n2 = "111111"
+
+    val parsedN1 = Integer.parseInt(n1, 2)
+    val parsedN2 = Integer.parseInt(n2, 2)
+
+    val result = BinaryArithmetic.sum(n1.toVector,n2.toVector).map(r => Integer.parseInt(r.mkString(""), 2))
+    result should be(Some(parsedN1 + parsedN2))
+
+  }
+
   "toDouble" should "convert a binary literal to its value as Double" in {
-    BinaryArithmetic.toDouble("000111".toCharArray) should be(Integer.parseInt("000111", 2))
-    BinaryArithmetic.toDouble("100011".toCharArray) should be(Integer.parseInt("100011", 2))
+    BinaryArithmetic.toDouble("000111".toVector) should be(Integer.parseInt("000111", 2))
+    BinaryArithmetic.toDouble("100011".toVector) should be(Integer.parseInt("100011", 2))
   }
 }
